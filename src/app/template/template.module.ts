77672c5fb5b7e8b723d991200c6e79eee17f7869
rafabrasail <rafaelrosenberg89@gmail.com>
2023-01-31 20:24:55 -0300

    init project

---
+++ src/app/template/template.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemplateModule { }
